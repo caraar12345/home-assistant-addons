f6f47290bfb3ebb0af7d43814802ebf573382dfa
Aaron Carson <aaron@aaroncarson.co.uk>
2022-02-26 19:12:35 +0000

    Docker is failing me

--- SuperQ_smokeping_prober/entrypoint.sh
+++ SuperQ_smokeping_prober/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 CONFIG_PATH="/data/options.json"
-echo "---"; cat tmp.json | yq e -P - > /data/config.yml
+echo "---"; cat $CONFIG_PATH | yq e -P - > /data/config.yml
 
-
+/usr/local/bin/smokeping_prober --config.file /data/config.yml 
