b33add730b906e3f5be35e135c8736eded07e754
Aaron Carson <aaron@aaroncarson.co.uk>
2022-02-27 17:09:44 +0000

    Testing now...

--- SuperQ_smokeping_prober/entrypoint.sh
+++ SuperQ_smokeping_prober/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 CONFIG_PATH="/data/options.json"
-echo "---"; cat $CONFIG_PATH | yq e -P - > /data/config.yml
+echo "---" | cat - $CONFIG_PATH | yq e -P - > /data/config.yml
 
-/usr/local/bin/smokeping_prober --config.file /data/config.yml 
+echo ----------
+echo options.json
+cat $CONFIG_PATH
+
+echo ----------
+echo config.yml
+cat /data/config.yml 
+
+echo ----------
+/usr/local/bin/smokeping_prober --config.file /data/config.json
